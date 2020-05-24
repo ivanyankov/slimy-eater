f6bce95e48ad69ea6e3dff9e5c65da5c27bc21a0
Ivan Yankov <ivan.yankov@rubberduck.xyz>
2020-05-25 00:18:28 +0300

    Create the game Start panel

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+
+    }
+
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/StartPanel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartPanel : MonoBehaviour
+{
+    public GameObject GameObject;
+    public InputField InputField;
+    public Text WarningText;
+    public string UsernameErrorMessage;
+    private bool ValidUsername = false;
+
+    public void HideStartPanel()
+    {
+        ValidateUsername();
+
+        if (this.ValidUsername == true) {
+            GameObject.SetActive(false);
+        }
+    }
+
+    public void ValidateUsername()
+    {
+        if (InputField.text == "") {
+            WarningText.text = UsernameErrorMessage;
+            WarningText.gameObject.SetActive(true);
+        } else {
+            WarningText.gameObject.SetActive(false);
+            this.ValidUsername = true;
+        }
+    }
+}
